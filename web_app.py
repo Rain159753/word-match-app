1279d2fe6b4e64b7a5ff3a3d911f3a3544423009
Rain159753 <67493228+Rain159753@users.noreply.github.com>
2025-12-06 22:59:13 +0800

    web_app.py

--- web_app.py
+++ web_app.py
@@ -268,7 +268,7 @@ footer_css = """
 }
 </style>
 <div class="footer">
-    <p>⚡ Powered by <b>Gemini & Zeno</b></p>
+    <p>⚡ Powered by <b>Gemini&Zeno</b></p>
 </div>
 """
 st.markdown(footer_css, unsafe_allow_html=True)
